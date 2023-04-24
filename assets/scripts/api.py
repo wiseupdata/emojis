b813b63ae8581060b9829cf229bdb664fa5b4ca0
silllvio <silllvio@hotmail.com>
2023-04-24 12:44:43 +0100

    sync

--- assets/scripts/api.py
+++ assets/scripts/api.py
@@ -9,7 +9,14 @@ emojis = response.json()
 COLS = 3
 
 # Create a list of emoji and code pairs
-pairs = [f':{name}: `:{name}:`' for name in emojis.keys()]
+pairs = [f':{name}: | `:{name}:`' for name in emojis.keys()]
+
+# Calculate the number of empty cells in the last row
+empty_cells = COLS - len(pairs) % COLS
+
+# Add empty cells to the pairs list if needed
+if empty_cells < COLS:
+    pairs += [''] * empty_cells
 
 # Group the pairs into sets
 chunks = [pairs[i:i+COLS] for i in range(0, len(pairs), COLS)]
@@ -20,10 +27,10 @@ rows = [f'| {" | ".join(chunk)} |' for chunk in chunks]
 row_join = '\n'.join(rows)
 
 # Create the header row
-header = '| ' + ' | '.join(['Emoji', 'Code'] * COLS) + ' |' + '\n| ' + ' | '.join(['---', '---'] * COLS) + ' |'
+header = '| ' + ' | '.join(['Emoji', 'Code'] * COLS) + ' |'
 
-# Create the markdown
-markdown = header + f"\n{row_join}"
+# Create the Markdown table
+markdown = f"{header}\n{'| ' + ' | '.join(['---', '---'] * COLS) + ' |'}\n{row_join}"
 
 # Save the Markdown to a file
 with open('emojis.md', 'w') as f:
