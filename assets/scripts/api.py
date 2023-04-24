c3b69b28c9c336d53d1a070fd432410958324b04
silllvio <silllvio@hotmail.com>
2023-04-24 12:40:07 +0100

    sync

--- assets/scripts/api.py
+++ assets/scripts/api.py
@@ -6,12 +6,24 @@ response = requests.get('https://api.github.com/emojis')
 # Get the emojis from the response
 emojis = response.json()
 
-# Create a Markdown table header
-markdown = '| Emoji Name | Emoji Code |\n| --- | --- |\n'
+COLS = 3
 
-# Loop through each emoji and add it to the Markdown table
-for emoji_name, emoji_url in emojis.items():
-    markdown += f'| :{emoji_name}: | `:{emoji_name}:` |\n'
+# Create a list of emoji and code pairs
+pairs = [f':{name}: `:{name}:`' for name in emojis.keys()]
+
+# Group the pairs into sets
+chunks = [pairs[i:i+COLS] for i in range(0, len(pairs), COLS)]
+
+# Convert the sets to rows in a Markdown table
+rows = [f'| {" | ".join(chunk)} |' for chunk in chunks]
+
+row_join = '\n'.join(rows)
+
+# Create the header row
+header = '| ' + ' | '.join(['Emoji', 'Code'] * COLS) + ' |' + '\n| ' + ' | '.join(['---', '---'] * COLS) + ' |'
+
+# Create the markdown
+markdown = header + f"\n{row_join}"
 
 # Save the Markdown to a file
 with open('emojis.md', 'w') as f:
